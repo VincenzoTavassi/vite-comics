aba8cc16ef4fe7f7a6130c250db5367a7c69fb16
VincenzoTavassi <info@egadivacanze.it>
2023-02-28 17:29:35 +0100

    final commit + font awesome test

--- src/main.js
+++ src/main.js
@@ -1,5 +1,23 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+// createApp(App).mount('#app')
+
+// FONT AWESOME
+
+/* import the fontawesome core */
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+/* import specific icons */
+import { faUserSecret } from "@fortawesome/free-solid-svg-icons";
+
+/* add icons to the library */
+library.add(faUserSecret);
+
+const app = createApp(App);
+app.component("font-awesome-icon", FontAwesomeIcon);
+app.mount("#app");
